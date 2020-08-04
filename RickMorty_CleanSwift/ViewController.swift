8ba6bbaa4be757d8061b6ab2a98e6a67a4f7076d
Nithi Kulasiriswatdi <kulasirswatdi@gmail.com>
2020-08-04 14:46:12 +0700

    no message

---
+++ RickMorty_CleanSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickMorty_CleanSwift
+//
+//  Created by Nithi Kulasiriswatdi on 4/8/2563 BE.
+//  Copyright © 2563 Nithi Kulasiriswatdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RickMorty_CleanSwiftTests/RickMorty_CleanSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  RickMorty_CleanSwiftTests.swift
+//  RickMorty_CleanSwiftTests
+//
+//  Created by Nithi Kulasiriswatdi on 4/8/2563 BE.
+//  Copyright © 2563 Nithi Kulasiriswatdi. All rights reserved.
+//
+
+import XCTest
+@testable import RickMorty_CleanSwift
+
+class RickMorty_CleanSwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
